2f3ce1c79866014177449693e3013b5a3ea707af
hugosenari <hugosenari@gmail.com>
2021-03-29 04:59:49 -0300

    feat: add balance types

--- iqoptionapi/rxioq/balances.py
+++ iqoptionapi/rxioq/balances.py
@@ -2,8 +2,28 @@ from enum import Enum, unique
 
 
 @unique
-class Color(Enum):
+class BalanceTypes(Enum):
     REAL = 1
     TOURNAMENT = 2
     PRATICE = 4
     CRYPTO = 5
+
+
+@unique
+class BalanceOrderChanged(Enum):
+    CFD = "cfd"
+    FOREX = "forex"
+    CRYPTO = "crypto"
+    DIGITAL_OPTION = "digital-option"
+    TURBO_OPTION = "turbo"
+    BINARY_OPTION = "binary"
+
+
+@unique
+class BalancePositionChanged(Enum):
+    CFD = "cfd"
+    CRYPTO = "crypto"
+    FOREX = "forex"
+    DIGITAL_OPTION = "digital-option"
+    BINARY_OPTION = "binary-option"
+    TURBO_OPTION = "turbo-option"
